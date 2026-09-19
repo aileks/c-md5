470bb03f4641e2993f9f6c62a4c7db992887ad7c
Kieran Pedrick <kieran@aileks.dev>
2026-09-19 15:18:43 -0400

    implement with hack-y global variable way

--- main.c
+++ main.c
@@ -11,12 +11,87 @@
 
 #define DEBUG(fmt, ...)                                                                            \
     do {                                                                                           \
-        fprintf(stderr, "[debug] ");                                                               \
-        fprintf(stderr, fmt, ##__VA_ARGS__);                                                       \
+        if (debug) {                                                                               \
+            fprintf(stderr, "[%s():%d] ", __func__, __LINE__);                                     \
+            fprintf(stderr, fmt, ##__VA_ARGS__);                                                   \
+        }                                                                                          \
     } while (false)
 
 uint64_t byte_count = 0;
 bool debug = false;
+uint8_t block[64] = {0};
+unsigned int block_idx = 0;
+uint32_t s[] = {
+    7,  12, 17, 22, 7,  12, 17, 22, 7,  12, 17, 22, 7,  12, 17, 22, 5,  9,  14, 20, 5,  9,
+    14, 20, 5,  9,  14, 20, 5,  9,  14, 20, 4,  11, 16, 23, 4,  11, 16, 23, 4,  11, 16, 23,
+    4,  11, 16, 23, 6,  10, 15, 21, 6,  10, 15, 21, 6,  10, 15, 21, 6,  10, 15, 21,
+};
+
+uint32_t K[] = {
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
+};
+
+void process_block()
+{
+    DEBUG("processing block\n");
+
+    uint32_t M[16];
+
+    // break block into 16 32-bit words M[i], 0 <= j <= 15
+    for (size_t i; i < 16; ++i) {
+        int j = i * 4;
+        M[i] = block[j] | block[j + 1] << 8 | block[j + 2] << 16 | block[j + 3] << 24;
+    }
+
+    // initialize hash val for block
+    uint32_t A = 0x67452301;
+    uint32_t B = 0xefcdab89;
+    uint32_t C = 0x98badcfe;
+    uint32_t D = 0x10325476;
+
+    for (size_t i; i < 64; ++i) {
+        uint32_t F;
+        uint32_t g;
+
+        if (i < 16) {
+            F = (B & C) | (~B & D);
+            g = i;
+        } else if (i < 32) {
+            F = (D & B) | (~D & C);
+            g = (5 * i + 1) % 16;
+        } else if (i < 48) {
+            F = B ^ C ^ D;
+            g = (3 * i + 5) % 16;
+        } else {
+            F = C ^ (B | ~D);
+        }
+
+        F = F + A + K[i] + M[g];
+        A = D;
+        D = C;
+        C = B;
+        B += F << s[i] | F >> (32 - s[i]);
+    }
+}
+
+void process_byte(uint8_t byte)
+{
+    DEBUG("processing byte: %u\n", byte);
+
+    block[block_idx++] = byte;
+
+    if (block_idx == 64) {
+        process_block();
+        block_idx = 0;
+    }
+}
 
 int process_input(int file_desc)
 {
@@ -41,8 +116,7 @@ int process_input(int file_desc)
 
         for (ssize_t i = 0; i < n; ++i) {
             uint8_t byte = buf[i];
-            /* process_byte(byte); */
-            DEBUG("processing byte: %u", byte);
+            process_byte(byte);
             byte_count++;
         }
 
